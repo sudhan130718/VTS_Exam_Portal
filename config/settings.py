d645d842ef9dd1255fc669f2c666a78dcb9530a9
MahaSudharson <sudhan1328@gmail.com>
2025-07-15 19:08:35 +0530

    add default db name

--- config/settings.py
+++ config/settings.py
@@ -89,7 +89,7 @@ from decouple import config
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
+        'NAME': config('DB_NAME', default='db.sqlite3'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
